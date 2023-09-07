b5fef75f31d5122b3cb15825f13e85abf20178e0
baptul99 <baptulchangdar@gmail.com>
2023-09-07 23:57:50 +0530

    bapyul

---
+++ todo-list/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Todo from "./todo/todo.js";
+// import Temp from "./components/weather/temp";
+
+const App = () => {
+  return (
+    <>
+      <Todo />
+    </>
+  );
+};
+
+export default App;
